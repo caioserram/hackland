9e5f85043380eba1e2ec486ed742e8a486f05e9b
llucaslleall <lucaslealsantos@poli.ufrj.br>
2021-01-16 06:42:33 -0300

    footer

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './home.css';
 
 import Navbar from '../../compenentes/navbar';
+import Footer from '../../compenentes/footer';
 
 function Home(){
     return(
+        <>
         <Navbar />
+        <Footer />
+        </>
     );
 }
 
