bb78beec3db29cea60688fe4422d2081bd7e2f80
llucaslleall <lucaslealsantos@poli.ufrj.br>
2021-01-16 06:35:39 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 /* PAGINAS */
 import Login from './view/login';
 import Cadastro from './view/cadastro';
+import Home from './view/home';
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={Login} />
+      <Route exact path="/" component={Home} />
       <Route exact path="/cadastro" component={Cadastro} />
+      <Route exact path="/home" component={Home} />
     </Router>
   );
 }
